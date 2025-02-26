62e7b3bdf1f5a5b12a341f4b6d00827d8e188c40
pentagon <alex-cvc@mail.ru>
2025-02-26 07:34:02 +0000

    add gripper joint

--- src/gripper_sim_gz/setup.py
+++ src/gripper_sim_gz/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "detached_sim_gz = gripper_sim_gz.detached_sim_gz:main"
+            'gripper_sim_gz = gripper_sim_gz.gripper_sim_gz:main'
         ],
     },
 )
