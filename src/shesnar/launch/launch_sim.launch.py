1a02cab2d4445e26d839ceb13a0648d85dbd5fd4
pentagon <alex-cvc@mail.ru>
2025-03-17 20:02:19 +0000

    Fix gz_bridge.yaml

--- src/shesnar/launch/launch_sim.launch.py
+++ src/shesnar/launch/launch_sim.launch.py
@@ -73,7 +73,7 @@ def generate_launch_description():
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot', "-z", '0.111' , "-y", '-0.3', "-x", '1.0' ],
+                                   '-name', 'my_bot', "-z", '0.111' , "-y", '-1.11', "-x", '0.76' ],
                         output='screen')
 
     bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
