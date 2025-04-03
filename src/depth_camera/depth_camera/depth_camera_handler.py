efa80bac20d9e78da305a49617f6ea566a609141
pentagon <alex-cvc@mail.ru>
2025-04-03 19:02:12 +0000

    depth camera new algorithm

--- src/depth_camera/depth_camera/depth_camera_handler.py
+++ src/depth_camera/depth_camera/depth_camera_handler.py
@@ -1,100 +1,69 @@
 import rclpy
-import numpy as np
 from rclpy.node import Node
-from sensor_msgs.msg import PointCloud2, LaserScan
-import sensor_msgs_py.point_cloud2 as pc2
-from scipy.spatial.transform import Rotation as R
-
+import cv2 as cv
 import numpy as np
-from sensor_msgs_py import point_cloud2 as pc2
-import time
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from std_msgs.msg import Float32MultiArray
+import math
 
-class PointCloudToLaserScan(Node):
+class BoardDetector(Node):
     def __init__(self):
-        super().__init__('pointcloud_to_laserscan')
-        self.subscription = self.create_subscription(
-            PointCloud2,
-            'robot_camera/depth_image_points',  # Измени на свой топик
-            self.pointcloud_callback,
-            10
-        )
-        self.scan_publisher = self.create_publisher(LaserScan, '/robot_camera/scan_layer', 10)
+        super().__init__('board_detector')
+        self.bridge = CvBridge()
+        # подключение к топику камеры
+        self.depth_sub = self.create_subscription(Image, '/robot_camera/depth_image', self.depth_callback, 10)
+        # топик с изображением для отладки
+        self.image_pub = self.create_publisher(Image, 'image_topic_2', 10)
 
-        self.camera_tilt_angle = -30.0  # Наклон камеры в градусах
-        self.rotation_matrix = R.from_euler('x', -self.camera_tilt_angle, degrees=True).as_matrix()
+    def depth_callback(self, msg):
+        # преобразование в читаемый cv формат
+        depth_image = self.bridge.imgmsg_to_cv2(msg, "32FC1")
+
+        # заменяем бесконечности на максимальное расстояние
+        depth_clipped = np.nan_to_num(depth_image, neginf=2.55, posinf=2.55) 
+
+        # в 32FC1 значение каждого пикселя это расстояние до объекта в метрах, меняем в сантиметры
+        depth_clipped = depth_clipped * 100
+        # преобразуем в юинт8 для работы с методами cv
+        depth_clipped = depth_clipped.astype(np.uint8)
+        # инвертируем для удобства работы
+        depth_clipped = cv.bitwise_not(depth_clipped)
+        # с ограничением с помощью дросселя плохо заработало
+        # ret,thresh4 = cv.threshold(depth_clipped,10,255,cv.THRESH_TOZERO)
+
+        # ищем контуры
+        edges = cv.Canny(depth_clipped, 20,20)
 
 
-    def pointcloud_callback(self, msg):
-        points = pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True)
-        #print(points)
-
-        # Define height layers (e.g., ground, mid, top)
-        height_layer = -0.14  # Modify based on your needs
-        scan_layer = []
-
-
-        #for x, y, z in points:
-        #    if height_layer - 0.02 <= z <= height_layer + 0.02:  # Allow small tolerance
-        #        scan_layer.append((x, y))
+        # для отладки
+        cdst = cv.cvtColor(depth_clipped, cv.COLOR_GRAY2BGR)
         
-        # выпонляется очень догло. Необходим более эффективное преобразование
-        scan_layer = [(x, y) for x, y, z in points if height_layer - 0.01 <= z <= height_layer + 0.01]
-
-        angle_of_best_fit_line(scan_layer)
-        
-        self.publish_scan(scan_layer, 0)
-        #time.sleep(0.5)
-
-
-
-    def publish_scan(self, points, height):
-        scan_msg = LaserScan()
-        scan_msg.header.stamp = self.get_clock().now().to_msg()
-        scan_msg.header.frame_id = 'base_link'
-        scan_msg.angle_min = -np.pi
-        scan_msg.angle_max = np.pi
-        scan_msg.angle_increment = np.pi / 1800.0  # 1° шаг
-        scan_msg.range_min = 0.1
-        scan_msg.range_max = 1.0
-        scan_msg.ranges = [float('inf')] * int((scan_msg.angle_max - scan_msg.angle_min) / scan_msg.angle_increment)
-
-        for x, y in points:
-            angle = np.arctan2(y, x)
-            distance = np.hypot(x, y)
-            index = int((angle - scan_msg.angle_min) / scan_msg.angle_increment)
-
-            if 0 <= index < len(scan_msg.ranges):
-                scan_msg.ranges[index] = min(scan_msg.ranges[index], distance)
-
-        self.scan_publisher.publish(scan_msg)
-
-
-def angle_of_best_fit_line(points):
-    """
-    Принимает список кортежей (x, y), аппроксимирует точки до прямой
-    и возвращает угол наклона этой прямой в градусах.
-    """
-    # Разбираем входные данные
-    x = np.array([p[0] for p in points])
-    y = np.array([p[1] for p in points])
-    
-    # Выполняем линейную регрессию (метод наименьших квадратов)
-    A = np.vstack([x, np.ones(len(x))]).T
-    m, _ = np.linalg.lstsq(A, y, rcond=None)[0]  # m - наклон прямой
-    
-    # Вычисляем угол в градусах
-    angle = np.degrees(np.arctan(m))
-    print('ange in degrees is', angle)
-
-    min_val = min(x**2 + y**2 for x, y in points)
-    print('distance is', min_val)
-    return angle
-
+        # ищем линии
+        lines = cv.HoughLines(edges, 1, np.pi / 180, 55, None, 0, 0)
+        # отрисовываем
+        if lines is not None:
+            for i in range(0, len(lines)):
+                rho = lines[i][0][0]
+                theta = lines[i][0][1]
+                #  убираем что не интерсено
+                if math.degrees(theta) < 80:
+                    a = math.cos(theta)
+                    b = math.sin(theta)
+                    print(math.degrees(theta))
+                    x0 = a * rho
+                    y0 = b * rho
+                    pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+                    pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+                    cv.line(cdst, pt1, pt2, (0,0,255), 1, cv.LINE_AA)
+            
+        print("---")
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(cdst))
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = PointCloudToLaserScan()
+    node = BoardDetector()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
--- src/shesnar/launch/launch_sim.launch.py
+++ src/shesnar/launch/launch_sim.launch.py
@@ -52,7 +52,8 @@ def generate_launch_description():
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot', "-z", '0.111' , "-y", '-0.3', "-x", '1.0' ],
+                                #    '-name', 'my_bot', "-z", '0.111' , "-y", '-0.3', "-x", '1.0' ],
+                                '-name', 'my_bot', "-z", '0.111' , "-y", '-1.1', "-x", '0.75' ],
                         output='screen')
 
     # mover = Node(
