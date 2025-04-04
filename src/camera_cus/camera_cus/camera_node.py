b1780b5f189a7d564e70f2e5d4e2557164bec9a5
StalkerK0t <vasyap8262@gmail.com>
2025-04-04 06:52:40 +0000

    aruco fix

--- src/camera_cus/camera_cus/camera_node.py
+++ src/camera_cus/camera_cus/camera_node.py
@@ -20,6 +20,20 @@ class ImageSubscriber(Node):
         parameters = cv2.aruco.DetectorParameters()
         self.detector = cv2.aruco.ArucoDetector(dictionary, parameters)
 
+        # Aruco positions in playground coordinates in format:
+        # <Aruco ID>: <corners in playground coordinates: top left, top right, bottom right, bottom left> 
+        Aruco = {
+            20: np.array([[550, 550, 0], [650, 550, 0], [650, 650, 0], [550, 650, 0]], dtype=np.float32),
+            21: np.array([[2350, 550, 0], [2450, 550, 0], [2450, 650, 0], [2350, 650, 0]], dtype=np.float32),
+            22: np.array([[550, 1350, 0], [650, 1350, 0], [650, 1450, 0], [550, 1450, 0]], dtype=np.float32),
+            23: np.array([[2350, 1350, 0], [2450, 1350, 0], [2450, 1450, 0], [2350, 1450, 0]], dtype=np.float32)
+        }
+        objPoints = np.fromiter(Aruco.values(), dtype=object)
+        ids = np.fromiter(Aruco.keys(), dtype=float)
+        print(objPoints, ids)
+        dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+        self.board = cv2.aruco.Board(objPoints, dictionary, ids)
+
     def image_callback(self, msg):
         try:
             image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
@@ -44,6 +58,14 @@ class ImageSubscriber(Node):
                     center = (int(top_left[0] + bottom_right[0]) // 2, int(top_left[1] + bottom_right[1]) // 2)
                     center = np.array(center)
                     result[ids[0]] = center
+
+            camera_matrix = np.array([[761.80910110473633, 0., 960], [0., 761.80913686752319, 540], [0., 0., 1.]], dtype=np.float32)
+            dist_coeffs = np.array([0, 0, 0, 0, 0], dtype=np.float32)
+            objPoints, imgPoints = self.board.matchImagePoints(marker_corners, marker_IDs)
+            retval, rvec, tvec = cv2.solvePnP(objPoints, imgPoints, camera_matrix, dist_coeffs)
+            self.get_logger().info(f"Rvec, tvec: {rvec, tvec}")
+            image = cv2.drawFrameAxes(image, camera_matrix, dist_coeffs, rvec, tvec, length=1000 )
+            self.get_logger().info(f"Coordinates: {result}")
             cv2.imshow("Camera Image", image)
             key = cv2.waitKey(1)
             if key == ord('q'):
