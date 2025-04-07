f2e1b39a5fc33d2d53d782cc4a417c3490b23c4f
pentagon <alex-cvc@mail.ru>
2025-04-07 17:52:57 +0300

    create coord handler

---
+++ src/route_controller/route_controller/coord_handler.py
@@ -0,0 +1,10 @@
+import math
+
+class CoordHandler:
+    @staticmethod
+    def point_1():
+        return (1.0, -1, math.radians(180)) 
+    
+    @staticmethod
+    def point_2():
+        return (2.0, -1, math.radians(-90)) 
