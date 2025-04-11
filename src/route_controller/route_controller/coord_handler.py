e5423ccbc86e1da8a170396b7199584ceaaefd2e
danilhhh <danvlay123@gmail.com>
2025-04-11 20:44:11 +0000

    Добавил точки

--- src/route_controller/route_controller/coord_handler.py
+++ src/route_controller/route_controller/coord_handler.py
@@ -3,8 +3,50 @@ import math
 class CoordHandler:
     @staticmethod
     def point_1():
-        return (1.0, -1, math.radians(180)) 
+        return (400, -215, math.radians(90)) 
     
     @staticmethod
     def point_2():
-        return (2.0, -1, math.radians(-90)) 
+        return (390, -775, math.radians(180)) 
+
+    @staticmethod
+    def point_3():
+        return (390, -2225, math.radians(180)) 
+    
+    @staticmethod
+    def point_4():
+        return (400, -2775, math.radians(-90)) 
+    
+    @staticmethod
+    def point_5():
+        return (810, -1100, math.radians(0)) 
+
+    @staticmethod
+    def point_6():
+        return (810, -1900, math.radians(0)) 
+
+    @staticmethod
+    def point_7():
+        return (1090, -1100, math.radians(180)) 
+
+    @staticmethod
+    def point_8():
+        return (1090, -1900, math.radians(180)) 
+
+    @staticmethod
+    def point_9():
+        return (1325, -215, math.radians(90)) 
+
+    @staticmethod
+    def point_10():
+        return (1325, -2775, math.radians(-90)) 
+
+    @staticmethod
+    def point_11():
+        return (1585, -825, math.radians(0)) 
+
+    @staticmethod
+    def point_12():
+        return (1585, -2175, math.radians(0)) 
+
+    
