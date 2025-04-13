86ae84597a39c8870ce9e7fbd408ba9e3d151cf7
hii <59565548+danilhhh@users.noreply.github.com>
2025-04-14 01:52:41 +0300

    Update coord_handler.py

--- src/route_controller/route_controller/coord_handler.py
+++ src/route_controller/route_controller/coord_handler.py
@@ -2,49 +2,49 @@ import math
 
 class CoordHandler:
     @staticmethod
-    def point_1():
+    def point_b1():
         return (0.400, -0.215, math.radians(90)) 
     
     @staticmethod
-    def point_2():
+    def point_b2():
         return (0.390, -0.775, math.radians(180)) 
 
     @staticmethod
-    def point_3():
+    def point_b3():
         return (0.390, -2.225, math.radians(180)) 
     
     @staticmethod
-    def point_4():
+    def point_b4():
         return (0.400, -2.775, math.radians(-90)) 
 
     @staticmethod
-    def point_5():
+    def point_b5():
         return (0.810, -1.100, math.radians(0)) 
     
     @staticmethod
-    def point_6():
+    def point_b6():
         return (0.810, -1.900, math.radians(0)) 
 
     @staticmethod
-    def point_7():
+    def point_b7():
         return (1.090, -1.100, math.radians(180)) 
     
     @staticmethod
-    def point_8():
+    def point_b8():
         return (1.090, -1.900, math.radians(180)) 
 
     @staticmethod
-    def point_9():
+    def point_b9():
         return (1.325, -0.215, math.radians(90)) 
     
     @staticmethod
-    def point_10():
+    def point_b10():
         return (1.325, -2.775, math.radians(-90)) 
 
     @staticmethod
-    def point_11():
+    def point_b11():
         return (1.585, -0.825, math.radians(0)) 
     
     @staticmethod
-    def point_12():
+    def point_b12():
         return (1.585, -2.175, math.radians(0)) 
