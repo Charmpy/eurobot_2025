89b2d9c34ff5105c003491bb37d79efeb8a92fa8
hii <59565548+danilhhh@users.noreply.github.com>
2025-04-14 01:42:22 +0300

    Update coord_handler.py

--- src/route_controller/route_controller/coord_handler.py
+++ src/route_controller/route_controller/coord_handler.py
@@ -3,8 +3,48 @@ import math
 class CoordHandler:
     @staticmethod
     def point_1():
-        return (1.0, -1, math.radians(180)) 
+        return (0.400, -0.215, math.radians(90)) 
     
     @staticmethod
     def point_2():
-        return (2.0, -1, math.radians(-90)) 
+        return (0.390, -0.775, math.radians(180)) 
+
+    @staticmethod
+    def point_3():
+        return (0.390, -2.225, math.radians(180)) 
+    
+    @staticmethod
+    def point_4():
+        return (0.400, -2.775, math.radians(-90)) 
+
+    @staticmethod
+    def point_5():
+        return (0.810, -1.100, math.radians(0)) 
+    
+    @staticmethod
+    def point_6():
+        return (0.810, -1.900, math.radians(0)) 
+
+    @staticmethod
+    def point_7():
+        return (1.090, -1.100, math.radians(180)) 
+    
+    @staticmethod
+    def point_8():
+        return (1.090, -1.900, math.radians(180)) 
+
+    @staticmethod
+    def point_9():
+        return (1.325, -0.215, math.radians(90)) 
+    
+    @staticmethod
+    def point_10():
+        return (1.325, -2.775, math.radians(-90)) 
+
+    @staticmethod
+    def point_11():
+        return (1.585, -0.825, math.radians(0)) 
+    
+    @staticmethod
+    def point_12():
+        return (1.585, -2.175, math.radians(0)) 
