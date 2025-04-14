e4aeb0d7b77017bc29e51f5b6068a03ea17835f5
StalkerK0t <vasyap8262@gmail.com>
2025-04-14 15:16:07 +0000

    refactoring1

--- src/camera_cus/camera_cus/camera_node.py
+++ src/camera_cus/camera_cus/camera_node.py
@@ -16,6 +16,10 @@ class ImageSubscriber(Node):
             10)
         self.bridge = CvBridge()
         self.get_logger().info('Image subscriber started')
+
+        self.camera_matrix = np.array([[761.80910110473633, 0., 960], [0., 761.80913686752319, 540], [0., 0., 1.]], dtype=np.float32)
+        self.dist_coeffs = np.array([0, 0, 0, 0, 0], dtype=np.float32)
+
         dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
         parameters = cv2.aruco.DetectorParameters()
         self.detector = cv2.aruco.ArucoDetector(dictionary, parameters)
@@ -30,69 +34,84 @@ class ImageSubscriber(Node):
         }
         objPoints = np.fromiter(Aruco.values(), dtype=object)
         ids = np.fromiter(Aruco.keys(), dtype=float)
-        print(objPoints, ids)
+        # print(objPoints, ids)
         dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
         self.board = cv2.aruco.Board(objPoints, dictionary, ids)
+        
+        self.is_calibrated = False
+
+    def transform(self, coord, z=0):
+        alpha = (-z + self.r[-1,:] @ self.t)/(self.r[-1,:] @ coord)
+        # self.get_logger().info(f"Matrix: \n{alpha}")
+        pose = self.r @ (alpha * coord - self.t)
+        pose[1] = 2000 - pose[1]
+        return pose[:2]
+
+    def find_markers(self):
+        marker_corners, marker_IDs, reject = self.detector.detectMarkers(self.image)
+        # self.get_logger().info(f"Founded markres with ids: {marker_IDs.ravel()}")
+        
+        if marker_corners:
+            result = {}
+            for ids, corners in zip(marker_IDs, marker_corners):
+                corners = corners.reshape(4, 2)
+                corners = corners.astype(int)
+                top_left = corners[0].ravel()
+                # top_right = corners[1].ravel()
+                bottom_right = corners[2].ravel()
+                # bottom_left = corners[3].ravel()
+                
+                cv2.putText(self.image, str(ids),
+                        (bottom_right[0], bottom_right[1] - 15),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (0, 255, 0), 2)
+
+                center = (int(top_left[0] + bottom_right[0]) // 2, int(top_left[1] + bottom_right[1]) // 2, 1)
+                center = np.array(center)
+                result[ids[0]] = center
+            return result, marker_corners, marker_IDs
+        else:
+            return None
+    
+    def calibrate(self):
+        pass
 
     def image_callback(self, msg):
         try:
-            image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            self.image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
             # cv2.imshow("Camera Image", image)
-            marker_corners, marker_IDs, reject = self.detector.detectMarkers(image)
-            self.get_logger().info(f"Founded markres with ids: {marker_IDs.ravel()}")
-            result = {}
-            if marker_corners:
-                for ids, corners in zip(marker_IDs, marker_corners):
-                    corners = corners.reshape(4, 2)
-                    corners = corners.astype(int)
-                    top_left = corners[0].ravel()
-                    # top_right = corners[1].ravel()
-                    bottom_right = corners[2].ravel()
-                    # bottom_left = corners[3].ravel()
-                    
-                    cv2.putText(image, str(ids),
-                            (bottom_right[0], bottom_right[1] - 15),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            0.5, (0, 255, 0), 2)
-
-                    center = (int(top_left[0] + bottom_right[0]) // 2, int(top_left[1] + bottom_right[1]) // 2, 1)
-                    center = np.array(center)
-                    result[ids[0]] = center
-
-            camera_matrix = np.array([[761.80910110473633, 0., 960], [0., 761.80913686752319, 540], [0., 0., 1.]], dtype=np.float32)
-            dist_coeffs = np.array([0, 0, 0, 0, 0], dtype=np.float32)
+            markers, marker_corners, marker_IDs = self.find_markers()
+            self.get_logger().info(f"Founded markres: {markers.keys()}")
+            
             objPoints, imgPoints = self.board.matchImagePoints(marker_corners, marker_IDs)
-            retval, rvec, tvec = cv2.solvePnP(objPoints, imgPoints, camera_matrix, dist_coeffs)
-            # self.get_logger().info(f"Tvec: {tvec}")
+            retval, rvec, tvec = cv2.solvePnP(objPoints, imgPoints, self.camera_matrix, self.dist_coeffs)
+            # self.get_logger().info(f"Tvec: \n{tvec}")
             # self.get_logger().info(f"Rvec: {rvec}")
-            # self.get_logger().info(f"Rmat: {cv2.Rodrigues(rvec)[0]}")
-            Rmat = cv2.Rodrigues(rvec)[0]
-            Rt = np.hstack((Rmat, tvec))
-            Transform = camera_matrix @ Rt
-            Rotate_inv, tvec_inv = Transform[:,:-1], Transform[:,-1]
-            Rotate = np.linalg.inv(Rotate_inv)
-            a = Rotate[-1,:]
-            # alpha = (0+a @ tvec_inv)/(a @ result[20])
+            # self.get_logger().info(f"Rmat: \n{cv2.Rodrigues(rvec)[0]}")
+            r_mat = cv2.Rodrigues(rvec)[0]
+            Rt = np.hstack((r_mat, tvec))
+            T = self.camera_matrix @ Rt
+            
+            self.r = np.linalg.inv(T[:,:-1])  # final rotate matrix
+            self.t = T[:,-1]  # final translate vector
+
             try:
-                alpha = (-435+a @ tvec_inv)/(a @ result[69])
-                # self.get_logger().info(f"Matrix: \n{alpha}")
-                pose = Rotate @ (alpha * result[69] - tvec_inv)
-                pose[1] = 2000 - pose[1]
-                self.get_logger().info(f"Pose: \n{pose[:2]}")
+                pose = self.transform(markers[69], 435)
+                self.get_logger().info(f"Pose: \n{pose}")
             except:
                 self.get_logger().warning(f"Marker not finded: {69}")
-            # self.get_logger().info(f"Matrix: \n{np.array(Rt, dtype=np.float16)}")
-            # self.get_logger().info(f"Matrix: \n{Transform @ Transform_inv}")
-            # self.get_logger().info(f"Matrix: \n{Transform_inv @ [600, 600, 0, 1]}")
+            # # self.get_logger().info(f"Matrix: \n{np.array(Rt, dtype=np.float16)}")
+            # # self.get_logger().info(f"Matrix: \n{Transform @ Transform_inv}")
+            # # self.get_logger().info(f"Matrix: \n{Transform_inv @ [600, 600, 0, 1]}")
 
-            image = cv2.drawFrameAxes(image, camera_matrix, dist_coeffs, rvec, tvec, length=1000)
-            # self.get_logger().info(f"Coordinates: {result}")
-            # cv2.imshow("Camera Image", image)
-            # key = cv2.waitKey(1)
-            # if key == ord('c'):
-            #     pass
-            # if key == ord('q'):
-            #     raise SystemExit
+            # image = cv2.drawFrameAxes(image, camera_matrix, dist_coeffs, rvec, tvec, length=1000)
+            # # self.get_logger().info(f"Coordinates: {result}")
+            # # cv2.imshow("Camera Image", image)
+            # # key = cv2.waitKey(1)
+            # # if key == ord('c'):
+            # #     pass
+            # # if key == ord('q'):
+            # #     raise SystemExit
 
         except Exception as e:
             self.get_logger().error(f'Error processing image: {e}')
