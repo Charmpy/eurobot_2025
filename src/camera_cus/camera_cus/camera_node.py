7b013ca9023ff6fc4a31cc306bec3fd45b0992f0
StalkerK0t <vasyap8262@gmail.com>
2025-04-15 10:42:30 +0000

    before merge

--- src/camera_cus/camera_cus/camera_node.py
+++ src/camera_cus/camera_cus/camera_node.py
@@ -5,6 +5,9 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 
+from geometry_msgs.msg import TransformStamped
+from tf2_ros import TransformBroadcaster
+
 class ImageSubscriber(Node):
     def __init__(self):
         
@@ -14,7 +17,10 @@ class ImageSubscriber(Node):
             '/camera/image', 
             self.image_callback,
             10)
+        self.tf_broadcaster = TransformBroadcaster(self)
+
         self.bridge = CvBridge()
+        
 
         self.get_logger().info(f"OpenCV version: {cv2.__version__}")
         self.get_logger().info('Image subscriber started')
@@ -100,6 +106,33 @@ class ImageSubscriber(Node):
         self.get_logger().info(f"t: {self.t}")
 
         self.is_calibrated = True
+
+    def send_tf(self, msg):
+        t = TransformStamped()
+
+        # Read message content and assign it to
+        # corresponding tf variables
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'world'
+        t.child_frame_id = 'base_link'
+
+        # Turtle only exists in 2D, thus we get x and y translation
+        # coordinates from the message and set the z coordinate to 0
+        t.transform.translation.x = msg.x
+        t.transform.translation.y = msg.y
+        t.transform.translation.z = 0.0
+
+        # For the same reason, turtle can only rotate around one axis
+        # and this why we set rotation in x and y to 0 and obtain
+        # rotation in z axis from the message
+        q = quaternion_from_euler(0, 0, msg.theta)
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+
+        # Send the transformation
+        self.tf_broadcaster.sendTransform(t)
 
     def image_callback(self, msg):
         self.image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
