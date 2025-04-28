bba513441bf89d54792ed793b3430f40455983a9
pentagon <alex-cvc@mail.ru>
2025-04-28 14:08:59 +0000

    2 cameras

--- src/depth_camera/depth_camera/depth_camera_handler.py
+++ src/depth_camera/depth_camera/depth_camera_handler.py
@@ -16,14 +16,15 @@ class BoardDetector(Node):
         super().__init__('board_detector')
         self.bridge = CvBridge()
         # подключение к топику камеры
-        self.depth_sub = self.create_subscription(Image, 'depth_right', self.depth_callback, 10)
+        self.depth_sub_left = self.create_subscription(Image, '/robot_camera_left/depth_image', self.depth_callback_left, 10)
+        self.depth_sub_right = self.create_subscription(Image, '/robot_camera_right/depth_image', self.depth_callback_right, 10)
 
         self.start_positioning_sub = self.create_subscription(String, 'positioning', self.position_calback, 10)
         self.positioning_pub = self.create_publisher(String, 'positioning', 10)
         # self.image_pub = self.create_publisher(Image, 'image_topic_2', 10)
 
         # топик с изображением для отладки
-        self.image_pub = self.create_publisher(Image, 'image_topic_2', 10)
+        self.image_pub_right = self.create_publisher(Image, 'image_topic_2', 10)
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
         self.prev_time = self.get_clock().now()
         self.integral = 0
@@ -33,11 +34,13 @@ class BoardDetector(Node):
         self.x_done = True
         self.y_done = True
         self.w_done = True
+        self.choose_algoritm = "stop"
 
         self.trouble_counter = 0
 
+
     def position_calback(self, msg):
-        if msg.data == "start":
+        if msg.data == "right" or msg.data == "left" or msg.data == "both":
             self.integral = 0
             self.x_integral = 0
             self.y_integral = 0
@@ -45,47 +48,42 @@ class BoardDetector(Node):
             self.x_done = False
             self.y_done = False
             self.w_done = False
+            self.choose_algoritm = msg.data
+        
         if msg.data == "stop":
             self.x_done = True
             self.y_done = True
             self.w_done = True
+            self.choose_algoritm = "stop"
 
 
-    def depth_callback(self, msg):
+
+    def depth_callback_left(self, msg):
         # преобразование в читаемый cv формат
         depth_image = self.bridge.imgmsg_to_cv2(msg, "32FC1")
-        depth_image = cv.rotate(depth_image, cv.ROTATE_180)
-
-        kernel = np.ones((3,3),np.float32)/25
-        blured = cv.filter2D(depth_image,-1,kernel)
 
         # заменяем бесконечности на максимальное расстояние
-        depth_clipped = np.nan_to_num(depth_image, neginf=255, posinf=255) 
+        depth_clipped = np.nan_to_num(depth_image, neginf=2.55, posinf=2.55) 
 
-        # в 32FC1 значение каждого пикселя это расстояние до объекта в метрах, меняем в сантиметры
-        # depth_clipped = depth_clipped * 100
         # преобразуем в юинт8 для работы с методами cv
+        depth_clipped = depth_clipped * 100
         depth_clipped = depth_clipped.astype(np.uint8)
         # инвертируем для удобства работы
         depth_clipped = cv.bitwise_not(depth_clipped)
-        # с ограничением с помощью дросселя плохо заработало
-        ret,thresh4 = cv.threshold(depth_clipped,170,255,cv.THRESH_BINARY)
+
+        # бинаризация
+        ret,thresh4 = cv.threshold(depth_clipped,230,255,cv.THRESH_BINARY)
         thresh4[:, -1] = 0
 
-
-        kernel = np.ones((7,7),np.uint8)
-        erosion = cv.erode(thresh4,kernel,iterations = 1)
-
-        opening = cv.morphologyEx(thresh4, cv.MORPH_OPEN, kernel)
-        # self.image_pub.publish(self.bridge.cv2_to_imgmsg(opening))
-
-
-        contours, hierarchy = cv.findContours(opening, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        # Поиск контуров деревяшки
+        contours, hierarchy = cv.findContours(thresh4, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         if len(contours) == 0:
+            # Левая не отвечает, ориентируемся по правой
+            self.choose_algoritm = "right"
             if self.trouble_counter < 5:
                 self.trouble_counter += 1
             elif self.trouble_counter == 5:
-                print("x5")
+                print("Нет картинка 5 кадров подряд")
                 msg = String()
                 msg.data = "error"
                 self.positioning_pub.publish(msg)
@@ -93,16 +91,19 @@ class BoardDetector(Node):
                 self.publisher_.publish(msg)
                 self.trouble_counter += 1
             return
+        self.trouble_counter = 0
+
+        # Ориентируемся по обеим камерам
+        if(self.choose_algoritm != "left"):
+            self.choose_algoritm = "both"
+
         cntsSorted = sorted(contours, key=lambda x: cv.contourArea(x))
         cnt = cntsSorted[-1]
         M = cv.moments(cnt)
-        if M['m00'] == 0:
-            return
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
         # print(cx, cy)
-        # ищем контуры
-        edges = cv.Canny(depth_clipped, 20,20)
+
         # для отладки
         cdst = cv.cvtColor(depth_clipped, cv.COLOR_GRAY2BGR)
         # cv.drawContours(cdst, contours, 0, (0,255,0), 3)
@@ -115,9 +116,12 @@ class BoardDetector(Node):
         f_2 = False
         f_3 = False
         f_4 = False
+
+        # аппроксимация
         approx = cv.approxPolyDP(cnt, eps * peri, True)
         cv.drawContours(cdst,[approx],0,(0,0,255),2)
         if len(approx) == 4:
+            # раскидование углов по положениям
             for i in approx:
                 if i[0][0] > cx and i[0][1] < cy and not f_1:
                     # print(i)
@@ -143,18 +147,130 @@ class BoardDetector(Node):
             if not f_1 or not f_2 or not f_3 or not f_4:
                 return
 
+            # Расчет ошибки
             dx = r_u[0][0] - l_u[0][0]
             dy = r_u[0][1] - l_u[0][1]
+            #print("left: ",r_u[0][0], l_u[0][0], r_u[0][1], l_u[0][1])
             
-            self.angle_error = -11.5 + math.degrees(math.atan(dy/dx))
-            self.y_error = - r_u[0][0] + 70
-            self.x_error = - r_u[0][1] + 55
-            # print(self.angle_error)
-            print(self.y_error, self.x_error)
+            self.angle_error_left = -2.73 - math.degrees(math.atan(dy/dx))
+            self.y_error_left = - l_u[0][0] + 14
+            self.x_error_left = - l_u[0][1] + 26
+            # print(self.error)
+            #print("left w, y, x: ",self.angle_error_left, self.y_error_left, self.x_error_left)
+
         else:
             return
-        self.image_pub.publish(self.bridge.cv2_to_imgmsg(cdst))
+        #self.image_pub_left.publish(self.bridge.cv2_to_imgmsg(cdst))
 
+        if not (self.x_done and self.y_done and self.w_done):
+            if not self.w_done:
+                self.angle_control()
+            self.linear_control()
+            print("---")
+
+    def depth_callback_right(self, msg):
+        # преобразование в читаемый cv формат
+        depth_image = self.bridge.imgmsg_to_cv2(msg, "32FC1")
+
+        # заменяем бесконечности на максимальное расстояние
+        depth_clipped = np.nan_to_num(depth_image, neginf=2.55, posinf=2.55) 
+
+        # преобразуем в юинт8 для работы с методами cv
+        depth_clipped = depth_clipped * 100
+        depth_clipped = depth_clipped.astype(np.uint8)
+        # инвертируем для удобства работы
+        depth_clipped = cv.bitwise_not(depth_clipped)
+
+        # бинаризация
+        ret,thresh4 = cv.threshold(depth_clipped,230,255,cv.THRESH_BINARY)
+        thresh4[:, -1] = 0
+
+        # Поиск контуров деревяшки
+        contours, hierarchy = cv.findContours(thresh4, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 0:
+            # Правая не отвечает, ориентируемся по левой
+            self.choose_algoritm = "left"
+            if self.trouble_counter < 5:
+                self.trouble_counter += 1
+            elif self.trouble_counter == 5:
+                print("Нет картинка 5 кадров подряд")
+                msg = String()
+                msg.data = "error"
+                self.positioning_pub.publish(msg)
+                msg = Twist()
+                self.publisher_.publish(msg)
+                self.trouble_counter += 1
+                
+            return
+        self.trouble_counter = 0
+
+        # Ориентируемся по обеим камерам
+        if(self.choose_algoritm != "right"):
+            self.choose_algoritm = "both"
+
+        cntsSorted = sorted(contours, key=lambda x: cv.contourArea(x))
+        cnt = cntsSorted[-1]
+        M = cv.moments(cnt)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        # print(cx, cy)
+
+        # для отладки
+        cdst = cv.cvtColor(depth_clipped, cv.COLOR_GRAY2BGR)
+        # cv.drawContours(cdst, contours, 0, (0,255,0), 3)
+        cv.circle(cdst,(cx, cy), 2, (0,255,255), -1)
+
+        peri = cv.arcLength(cnt, True)
+
+        eps = 0.1
+        f_1 = False
+        f_2 = False
+        f_3 = False
+        f_4 = False
+
+        # аппроксимация
+        approx = cv.approxPolyDP(cnt, eps * peri, True)
+        cv.drawContours(cdst,[approx],0,(0,0,255),2)
+        if len(approx) == 4:
+            # раскидование углов по положениям
+            for i in approx:
+                if i[0][0] > cx and i[0][1] < cy and not f_1:
+                    # print(i)
+                    r_u = i
+                    cv.circle(cdst, r_u[0], 5, (0,255,255), -1)
+                    f_1 = True
+                
+                if i[0][0] > cx and i[0][1] > cy and not f_2:
+                    # print(i)
+                    r_d = i
+                    cv.circle(cdst, r_d[0], 5, (0,255,255), -1)
+                    f_2 = True
+                if i[0][0] < cx and i[0][1] > cy and not f_3:
+                    # print(i)
+                    l_d = i
+                    cv.circle(cdst, l_d[0], 5, (0,255,255), -1)
+                    f_3 = True
+                if i[0][0] < cx and i[0][1] < cy and not f_4:
+                    # print(i)
+                    l_u = i
+                    cv.circle(cdst, l_u[0], 5, (0,255,255), -1)
+                    f_4 = True
+            if not f_1 or not f_2 or not f_3 or not f_4:
+                return
+
+            # Расчет ошибки
+            dx = r_u[0][0] - l_u[0][0]
+            dy = -(r_u[0][1] - l_u[0][1])
+            #print("right: ",r_u[0][0], l_u[0][0], r_u[0][1], l_u[0][1])
+            
+            self.angle_error_right = 3.04 + math.degrees(math.atan(dy/dx))
+            self.y_error_right = - r_u[0][0] + 94
+            self.x_error_right = - r_u[0][1] + 26
+            # print(self.error)
+            #print("right w, y, x: ",self.angle_error_right, self.y_error_right, self.x_error_right)
+        else:
+            return
+        self.image_pub_right.publish(self.bridge.cv2_to_imgmsg(cdst))
 
         if not (self.x_done and self.y_done and self.w_done):
             if not self.w_done:
@@ -163,13 +279,43 @@ class BoardDetector(Node):
             print("---")
 
 
+
+    # Отработка линейной ошибки    
     def linear_control(self):
-        ki = 0.005
-        kp = 0.0005
+        if(self.choose_algoritm == "left"):
+            self.x_error = self.x_error_left
+            self.y_error = self.y_error_left
+        elif(self.choose_algoritm == "right"):
+            self.x_error = self.x_error_right
+            self.y_error = self.y_error_right
+        elif(self.choose_algoritm == "both"):
+            self.x_error = (self.x_error_left + self.x_error_right)/2
+            self.y_error = (self.y_error_left + self.y_error_right)/2
+        # Коэффициенты регулятора
+        ki = 0.001
+        kp = 0.005
         dt = self.get_clock().now().nanoseconds - self.prev_time.nanoseconds
         dt = dt * (10**-9)
 
-        if self.w_done: 
+        # Начало работы линейной после угловой    
+        if self.w_done:
+            #print(self.choose_algoritm) 
+            if abs(self.x_error) > 1:
+                self.x_integral += (dt * self.x_error)
+                if self.x_integral * ki  > 0.05:
+                    self.x_integral = 0.05 / ki
+                
+                msg = Twist()
+                msg.linear.x = self.x_error * kp + self.x_integral * ki
+                print(f'x упр: {self.x_error * kp + self.x_integral * ki}')
+                self.publisher_.publish(msg)
+            else:
+                if not self.x_done:
+                    msg = Twist()
+                    self.publisher_.publish(msg)
+                    self.x_done = True
+                    print("done x")
+                    self.x_integral = 0
 
             if abs(self.y_error) > 3:
                 self.y_integral += (dt * self.y_error)
@@ -187,31 +333,20 @@ class BoardDetector(Node):
                     self.publisher_.publish(msg)
                     self.y_done = True
                     self.y_integral = 0
-
-            if abs(self.x_error) > 3:
-                self.x_integral += (dt * self.x_error)
-                if self.x_integral * ki  > 0.05:
-                    self.x_integral = 0.05 / ki
-                
-                msg = Twist()
-                msg.linear.x = self.x_error * kp + self.x_integral * ki
-                print(f'x упр: {self.x_error * kp + self.x_integral * ki}')
-                self.publisher_.publish(msg)
-            else:
-                if not self.x_done:
-                    msg = Twist()
-                    self.publisher_.publish(msg)
-                    self.x_done = True
-                    print("done x")
-                    self.x_integral = 0
-
-            
             self.prev_time = self.get_clock().now()
 
-        
+    # угловая ошибка
     def angle_control(self):
-        ki = 0.0005
-        kp = 0.002
+        #print(self.choose_algoritm)
+        if(self.choose_algoritm == "left"):
+            self.angle_error = self.angle_error_left
+        elif(self.choose_algoritm == "right"):
+            self.angle_error = self.angle_error_right
+        elif(self.choose_algoritm == "both"):
+            self.angle_error = (self.angle_error_left + self.angle_error_right)/2
+        # коэффицифенты регулятора
+        ki = 0.005
+        kp = 0.02
         dt = self.get_clock().now().nanoseconds - self.prev_time.nanoseconds
         dt = dt * (10**-9)
 
@@ -225,7 +360,7 @@ class BoardDetector(Node):
             if w_con > 0.5:
                 w_con = 0.5
             msg = Twist()
-            msg.angular.z = -w_con
+            msg.angular.z = w_con
             print(f'w упр: {w_con}')
             self.publisher_.publish(msg)
         else:
