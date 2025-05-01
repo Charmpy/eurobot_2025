d5fcba33d4857c3738e06f6cb1a6de5b4fcd192a
Charmpy <alex-cvc@mail.ru>
2025-05-01 13:04:05 +0000

    added timer

--- src/depth_camera/depth_camera/depth_camera_handler.py
+++ src/depth_camera/depth_camera/depth_camera_handler.py
@@ -47,6 +47,8 @@ class BoardDetector(Node):
         self.x_error_right = 0
         self.y_error_right = 0
         self.angle_error_right = 0
+
+        self.binar_min = 177
         
 
 
@@ -89,7 +91,7 @@ class BoardDetector(Node):
         # инвертируем для удобства работы
         depth_clipped = cv.bitwise_not(depth_clipped)
         # с ограничением с помощью дросселя плохо заработало
-        ret,thresh4 = cv.threshold(depth_clipped,178,255,cv.THRESH_BINARY)
+        ret,thresh4 = cv.threshold(depth_clipped,self.binar_min,255,cv.THRESH_BINARY)
         thresh4[:, -1] = 0
 
 
@@ -234,7 +236,7 @@ class BoardDetector(Node):
         # инвертируем для удобства работы
         depth_clipped = cv.bitwise_not(depth_clipped)
         # с ограничением с помощью дросселя плохо заработало
-        ret,thresh4 = cv.threshold(depth_clipped,178,255,cv.THRESH_BINARY)
+        ret,thresh4 = cv.threshold(depth_clipped,self.binar_min,255,cv.THRESH_BINARY)
         thresh4[:, -1] = 0
 
 
@@ -368,39 +370,23 @@ class BoardDetector(Node):
 
 
         # Коэффициенты регулятора
-        ki = 0.003
-        kp = 0.0002
+        ki_x = 0.003
+        kp_x = 0.0002
+        ki_y = 0.002
+        kp_y = 0.0002
         dt = self.get_clock().now().nanoseconds - self.prev_time.nanoseconds
         dt = dt * (10**-9)
 
         # Начало работы линейной после угловой    
         if self.w_done:
-            #print(self.choose_algoritm) 
-            if abs(self.x_error) > 3:
-                self.x_integral += (dt * self.x_error)
-                if self.x_integral * ki  > 0.05:
-                    self.x_integral = 0.05 / ki
-                
-                msg = Twist()
-                msg.linear.x = self.x_error * kp + self.x_integral * ki
-                print(f'x упр: {self.x_error * kp + self.x_integral * ki}')
-                self.publisher_.publish(msg)
-            else:
-                if not self.x_done:
-                    msg = Twist()
-                    self.publisher_.publish(msg)
-                    self.x_done = True
-                    print("done x")
-                    self.x_integral = 0
-
             if abs(self.y_error) > 3:
                 self.y_integral += (dt * self.y_error)
-                if self.y_integral * ki  > 0.05:
-                    self.y_integral = 0.05 / ki
+                if self.y_integral * ki_y  > 0.04:
+                    self.y_integral = 0.04 / ki_y
                 
                 msg = Twist()
-                msg.linear.y = self.y_error * kp + self.y_integral * ki
-                print(f'y упр: {self.y_error * kp + self.y_integral * ki}')
+                msg.linear.y = self.y_error * kp_y + self.y_integral * ki_y
+                #print(f'y упр: {self.y_error * kp + self.y_integral * ki}')
                 self.publisher_.publish(msg)
             else:
                 if not self.y_done:
@@ -410,6 +396,26 @@ class BoardDetector(Node):
                     self.y_done = True
                     self.y_integral = 0
             self.prev_time = self.get_clock().now()
+
+            #print(self.choose_algoritm) 
+            if abs(self.x_error) > 3:
+                self.x_integral += (dt * self.x_error)
+                if self.x_integral * ki_x  > 0.05:
+                    self.x_integral = 0.05 / ki_x
+                
+                msg = Twist()
+                msg.linear.x = self.x_error * kp_x + self.x_integral * ki_x
+                print(f'x упр: {self.x_error * kp_y + self.x_integral * ki_x}')
+                self.publisher_.publish(msg)
+            else:
+                if not self.x_done:
+                    msg = Twist()
+                    self.publisher_.publish(msg)
+                    self.x_done = True
+                    print("done x")
+                    self.x_integral = 0
+
+    
 
     # угловая ошибка
     def angle_control(self):
