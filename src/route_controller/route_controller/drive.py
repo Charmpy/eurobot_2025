a14ecd68139379636d2f71fd52aa51de24ac1198
Charmpy <alex-cvc@mail.ru>
2025-05-04 08:44:41 +0000

    drive

--- src/route_controller/route_controller/drive.py
+++ src/route_controller/route_controller/drive.py
@@ -69,8 +69,13 @@ def go_to_goal(navigator, coords, goal_type):
 
 def main(args=None):
     rclpy.init(args=args)
+    logger = rclpy.logging.get_logger('aboba')
+    rclpy.logging.set_logger_level('aboba', rclpy.logging.LoggingSeverity.DEBUG)
+    logger.debug("Start")
+
     RM = RobotMacros()
     RM.com_start_state()
+    logger.debug("First start state, WAIT FOR KEY")
 
     start_listener = StartListener()
 
@@ -84,11 +89,11 @@ def main(args=None):
         time.sleep(0.1)
 
     time.sleep(0.1)
-    RM.com_start_state()
-    time.sleep(0.1)
-    RM.time_move(0.1, 5)
-    time.sleep(0.1)
-    RM.time_move(-0.1, 5)
+    # RM.com_start_state()
+    # time.sleep(0.1)
+    # RM.time_move(0.1, 5)
+    # time.sleep(0.1)
+    # RM.time_move(-0.1, 5)
     # RM.com_compile()
     # time.sleep(0.1)
     # RM.time_move(0.05, 0.5)
@@ -99,32 +104,119 @@ def main(args=None):
     # time.sleep(0.1)
     # RM.com_start_state()
     # time.sleep(0.1)
-    logger = rclpy.logging.get_logger('aboba')
-    rclpy.logging.set_logger_level('aboba', rclpy.logging.LoggingSeverity.DEBUG)
-    logger.debug("Start")
+
+    # ======= Wotking miniprogramm ===========
+    # time.sleep(0.5)
+    # RM.place_flag()
+    # logger.debug("поставил флаг")
+    # time.sleep(0.5)
+
+    # RM.time_move(0.05, 5)
+    # time.sleep(0.5)
+    # # RM.time_move(-0.1, 5)
+
+    # logger.debug("собираю")
+    # RM.com_compile()
+    # time.sleep(2.5)
+    # logger.debug("собрал")
+    # RM.time_move(-0.05, 2)
+    # time.sleep(0.1)
+
+    # logger.debug("строю")
+    # RM.com_build()
+    # time.sleep(2)    
+    # logger.debug("построил")
+    # RM.time_move(-0.05, 4)
+    # time.sleep(0.1)
+
+    # logger.debug("Last start state")
+    # RM.com_start_state()
+    # time.sleep(0.5)
+    # ======= Wotking miniprogramm ===========
+
+    
 
     navigator = BasicNavigator()
-    coords = CoordHandler()
+    # coords = CoordHandler()
 
-    all_storages_complete = False
-    all_goals_complete = False
+    # all_storages_complete = False
+    # all_goals_complete = False
 
+    time.sleep(1)
+    while True:
+        logger.debug("Я в цикле")
+
+
+        ## подъезд к трибуне
+        time_ = navigator.get_clock().now().to_msg()
+        goal_pose = Navi.set_goal_pose(0.65, -1.04, 0, time_)
+        logger.debug("Я поехал 1")
+        navigator.goToPose(goal_pose)
+        while not navigator.isNavComplete():
+            # if navigator.getResult() != NavigationResult.FAILED:
+            #     logger.debug("HUUUUUIII")
+            #     break
+            continue
+        logger.debug("Я приехал 1")
+
+        ## проезжаем немного вперед
+        time_ = navigator.get_clock().now().to_msg()
+        goal_pose = Navi.set_goal_pose(0.80, -1.04, 0, time_)
+        logger.debug("Я поехал 2")
+        # RM.com_position()
+        navigator.goToPose(goal_pose)
+        while not navigator.isNavComplete():
+            continue
+        logger.debug("Я приехал 2")
+
+        ## собираем трибуну
+        logger.debug("собираю")
+        RM.com_compile()
+        time.sleep(2.5)
+        logger.debug("собрал")
+
+
+        # ## разворачиваемся
+        # time_ = navigator.get_clock().now().to_msg()
+        # goal_pose = Navi.set_goal_pose(0.80, -1.05, 180, time_)
+        # logger.debug("Я разворачиваюсь")
+        # navigator.goToPose(goal_pose)
+        # while not navigator.isNavComplete():
+        #     continue
+        # logger.debug("Я развернулся")
+
+        # едем с банками в зону
+        time_ = navigator.get_clock().now().to_msg()
+        goal_pose = Navi.set_goal_pose(0.25, -1.25, 180, time_)
+        logger.debug("Я поехал 3")
+        # navigator.goToPose(goal_pose)
+        # while not navigator.isNavComplete():
+        #     continue
+        logger.debug("Я приехал 3")
+        RM.com_build()
+        time.sleep(4)
+
+        RM.time_move(-0.1, 5)
+        RM.com_start_state()
+
+        break
     # navigator = Navi()    
-    # navigator.configure_init_pose(1.0, -0.3, 0.111)
-    # while True:
-    #     time.sleep(0.1)
-    #     RM.com_start_state()
-    #     time.sleep(2.0)
-    #     logger.debug("Start")
-
-    #     RM.time_move(-0.1,1.0)
-    #     time.sleep(5.0)
+    # # navigator.configure_init_pose(1.0, -0.3, 0.111)
+    # # while True:
+    # #     time.sleep(0.1)
+    # #     RM.com_start_state()
+    # #     time.sleep(2.0)
+    # #     logger.debug("Start")
+    # # go_to_goal(H)
+    # #     RM.time_move(-0.1,1.0)
+    # #     time.sleep(5.0)
     # while not (all_storages_complete or all_goals_complete):
-
-    #     goal_type = "storage"
-    #     if not go_to_goal(navigator, coords, goal_type):
-    #         all_storages_complete = True
-    #         print("all_storages_complete")
+    #     goal = set_goal_pose(navigator, coords, goal_type)
+    # goal_pose = Nav.set_goal_pose
+    #     # goal_type = "storage"
+    #     # if not go_to_goal(navigator, coords, goal_type):
+    #     #     all_storages_complete = True
+    #     #     print("all_storages_complete")
 
     #     logger.debug("Я еду")
     #     while not navigator.isNavComplete():
